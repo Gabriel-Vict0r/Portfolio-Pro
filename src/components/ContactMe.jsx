e3b701964f2b780e2f5804dbf850b23a505a2a09
Gabriel-Vict0r <victorgabriel1730@gmail.com>
2023-03-14 12:38:08 -0300

    feat
    form validation, used: react-hook-forms and Yup

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -2,18 +2,41 @@ import React from "react";
 import "../sass/components/widgets/contactMe.sass";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import InputMask from "react-input-mask";
 
+const schema = yup.object({
+  name: yup
+    .string("O campo deve conter apenas letras")
+    .required("O nome é obrigatório"),
+  email: yup
+    .string()
+    .email("Digite um email válido")
+    .required("O email é obrigatório"),
+  phone: yup.string().required("O telefone é obrigatório"),
+  subject: yup
+    .string()
+    .min(2, "o assunto deve ter no mínimo duas letras")
+    .required("O assunto é obrigatório"),
+  message: yup
+    .string()
+    .min(2, "A mensagem deve ter no mínimo duas letras")
+    .required("A mensagem é obrigatória"),
+});
 const ContactMe = () => {
-  const { register, handleSubmit } = useForm();
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [subject, setSubject] = useState("");
-  const [phone, setPhone] = useState("");
-  const [message, setMessage] = useState("");
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
 
-  function sendEmail(e) {
-    e.preventDefault();
+  function sendEmail(data) {
+    console.log(data);
   }
+  console.log(errors);
   return (
     <section className="contact">
       <img
@@ -24,63 +47,60 @@ const ContactMe = () => {
       <div className="light-form" />
       <form className="contact-form" onSubmit={handleSubmit(sendEmail)}>
         <h2>Contate-me</h2>
-        <input
-          type="text"
-          name="name"
-          className="contact-input"
-          placeholder="Nome"
-          ref={register({
-            required: true,
-            maxLengh: 80,
-            pattern: /^[A-Za-z]+$/i,
-          })}
-          // onChange={(e) => setName(e.target.value)}
-          // value={name}
-        />
-        <input
-          type="email"
-          name="email"
-          id=""
-          className="contact-input"
-          placeholder="E-mail"
-          ref={register({required: true, maxLengh: 80})}
-          // onChange={(e) => setEmail(e.target.value)}
-          // value={email}
-        />
-        <input
-          type="text"
-          name="assunto"
-          id=""
-          className="contact-input"
-          placeholder="Assunto"
-          ref={register({required: true, maxLengh: 100})}
-          // onChange={(e) => setSubject(e.target.value)}
-          // value={subject}
-        />
-        <input
-          type="tel"
-          name="telefone"
-          className="contact-input"
-          placeholder="Celular"
-          onChange={(e) => setPhone(e.target.value)}
-          value={phone}
-        />
-        <textarea
-          name="mensagem"
-          id=""
-          cols="30"
-          rows="3"
-          placeholder="Mensagem"
-          onChange={(e) => setMessage(e.target.value)}
-          value={message}
-        ></textarea>
-        <input
-          type="submit"
-          name="enviar"
-          id=""
-          className="button-send"
-          value="Enviar"
-        />
+        <div className="field">
+          <input
+            type="text"
+            className="contact-input"
+            placeholder="Nome"
+            {...register("name")}
+          />
+          <span className="errorMessage">{errors.name?.message}</span>
+        </div>
+        <div className="field">
+          <input
+            type="text"
+            className="contact-input"
+            placeholder="E-mail"
+            {...register("email")}
+          />
+          <span className="errorMessage">{errors.email?.message}</span>
+        </div>
+
+        <div className="field">
+          <input
+            type="text"
+            id=""
+            className="contact-input"
+            placeholder="Assunto"
+            {...register("subject")}
+          />
+          <span className="errorMessage msgSubject">
+            {errors.subject?.message}
+          </span>
+        </div>
+        <div className="field">
+          <InputMask
+            type="text"
+            className="contact-input"
+            placeholder="Telefone"
+            mask="(99) 99999-9999"
+            {...register("phone")}
+          />
+          <span className="errorMessage msgSubject">
+            {errors.phone?.message}
+          </span>
+        </div>
+        <div className="field">
+          <textarea
+            id=""
+            cols="30"
+            rows="3"
+            placeholder="Mensagem"
+            {...register("message")}
+          ></textarea>
+          <span className="errorMessage msgMessage">{errors.message?.message}</span>
+        </div>
+        <input type="submit" className="button-send" value="Enviar" />
       </form>
     </section>
   );
